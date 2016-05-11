1255b2f2c03fbc3989aabf9a78602c82d8b74e7d
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-12 01:00:38 +0300

    RED: Create a failing test for top priority todos

--- test/integration/listing_todos_test.rb
+++ test/integration/listing_todos_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ListingTodosTest < ActionDispatch::IntegrationTest
   setup do
     Todo.create!(title: 'Buy bread and butter', priority: 1)
+    Todo.create!(title: 'Wash the dishes', priority: 3)
     Todo.create!(title: 'Watch the game tonight', priority: 5)
   end
 
@@ -13,5 +14,14 @@ class ListingTodosTest < ActionDispatch::IntegrationTest
     assert_equal Mime::JSON, response.content_type
 
     assert_equal Todo.count, JSON.parse(response.body).size
+  end
+
+  test 'list top priority todos' do
+    get '/todos?priority=5'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    assert_equal 1, JSON.parse(response.body).size
   end
 end
