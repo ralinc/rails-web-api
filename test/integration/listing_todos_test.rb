9bd90ba8304024d688b67de519998d4cd7aadab9
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-12 00:32:16 +0300

    RED: Create a failing test for all todos count

--- test/integration/listing_todos_test.rb
+++ test/integration/listing_todos_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class ListingTodosTest < ActionDispatch::IntegrationTest
+  setup do
+    Todo.create!(title: 'Buy bread and butter', priority: 1)
+    Todo.create!(title: 'Watch the game tonight', priority: 5)
+  end
+
   test 'listing todos' do
     get '/todos'
+
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
+
+    assert_equal Todo.count, JSON.parse(response.body).size
   end
 end
