9c507e11080f614b23cdba97a51f938766d65ae1
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-12 01:06:04 +0300

    GREEN: Implement the priority filter for todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,11 @@
 class TodosController < ApplicationController
   def index
-    render json: Todo.all, status: 200
+    todos = Todo.all
+
+    if priority = params[:priority]
+      todos = todos.where(priority: priority)
+    end
+
+    render json: todos, status: 200
   end
 end
