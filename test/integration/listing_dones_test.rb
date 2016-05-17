2c8366200a9fd0e0aab439f4cbbd4479447018dc
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-17 20:15:39 +0300

    RED: Make the listing completed todos test for real
    Make the listing dones test actually test for listing ONLY todos
    which are done. I will mark todos as done by adding a date to the
    model which states when the todo was finished.

--- test/integration/listing_dones_test.rb
+++ test/integration/listing_dones_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class ListingDonesTest < ActionDispatch::IntegrationTest
+  setup do
+    Todo.create!(title: "Paint the fence", done_at: 1.day.ago)
+    Todo.create!(title: "Feed the cat", done_at: nil)
+  end
+
   test 'list dones (finished todos)' do
     get '/dones', {}, { 'Accept' => 'application/json' }
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
+    assert_equal 1, json(response.body).size
   end
 end
