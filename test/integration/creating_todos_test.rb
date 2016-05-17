eb262c129b4a0b7dd2ddaf4e011646ec5f4ba059
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-17 21:33:38 +0300

    RED: Create a failing test for creating todos

---
+++ test/integration/creating_todos_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class CreatingTodosTest < ActionDispatch::IntegrationTest
+  test 'creates new todos' do
+    post '/todos', {
+        todo: {
+          title: 'Buy extra chocolate',
+          priority: 5
+        }
+      }.to_json,
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+
+    assert_equal 201, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    todo = json(response.body)
+    assert_equal todo_url(todo[:id]), response.location
+  end
+end
