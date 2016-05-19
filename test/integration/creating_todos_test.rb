7886044b98049e809e4e3d3c70c7ebf793addb0a
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-19 21:31:10 +0300

    RED: Create a failing test for handling invalid json requests

--- test/integration/creating_todos_test.rb
+++ test/integration/creating_todos_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CreatingTodosTest < ActionDispatch::IntegrationTest
-  test 'creates new todos' do
+  test 'creates new todos with valid json' do
     post '/todos', {
         todo: {
           title: 'Buy extra chocolate',
@@ -21,5 +21,20 @@ class CreatingTodosTest < ActionDispatch::IntegrationTest
 
     assert_equal 'Buy extra chocolate', todo[:title]
     assert_equal 5, todo[:priority]
+  end
+
+  test 'does not create todos with invalid json' do
+    post '/todos', {
+        todo: {
+          title: nil,
+          priority: 5
+        }
+      }.to_json,
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+
+    assert_equal 422, response.status
   end
 end
